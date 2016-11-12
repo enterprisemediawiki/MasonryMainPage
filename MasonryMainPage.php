35f1f9e49a4b80476ee553801ef61cd6d9ebafdb
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-11-12 08:46:02 -0600

    Upgrade for new versions MW: extension registration and i18n
    * Modify MasonryMainPage.php to check for wfLoadExtension
      and use it if available
    * Add extension.json to be used by wfLoadExtension
    * Move i18n to /i18n directory and JSON files
    * Leave magic word registration in its own i18n file, but
      rename to Magic.php

--- MasonryMainPage.php
+++ MasonryMainPage.php
@@ -18,48 +18,48 @@ if (!defined('MEDIAWIKI')) {
 	die( "MasonryMainPage extension" );
 }
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'MasonryMainPage',
-	'url'            => 'http://github.com/enterprisemediawiki/MasonryMainPage',
-	'author'         => '[https://www.mediawiki.org/wiki/User:Darenwelsh Daren Welsh]',
-	'descriptionmsg' => 'masonrymainpage-desc',
-	'version'        => '0.2.1'
-);
+// if wfLoadExtension exists (MW 1.25+) use that method of registering extensions
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MasonryMainPage' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['MasonryMainPage'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['MasonryMainPageMagic'] = __DIR__ . '/Magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for MasonryMainPage extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
 
-# $dir: the directory of this file, e.g. something like:
-#	1)	/var/www/wiki/extensions/BlankParserFunction
-# 	2)	C:/xampp/htdocs/wiki/extensions/BlankParserFunction
-$dir = dirname( __FILE__ ) . '/';
+// For MW 1.24 and lower, use normal extension registration
+// This should be removed when this extension drops support for <1.25
+} else {
 
-# Internationalization
-$wgExtensionMessagesFiles['MasonryMainPage'] = $dir . 'MasonryMainPage.i18n.php';
+	$wgExtensionCredits['parserhook'][] = array(
+		'path'           => __FILE__,
+		'name'           => 'MasonryMainPage',
+		'url'            => 'http://github.com/enterprisemediawiki/MasonryMainPage',
+		'author'         => '[https://www.mediawiki.org/wiki/User:Darenwelsh Daren Welsh]',
+		'descriptionmsg' => 'masonrymainpage-desc',
+		'version'        => '0.3.0'
+	);
 
-# The "class" file contains the bulk of a simple parser function extension.
-$wgAutoloadClasses['MasonryMainPage'] = $dir . 'MasonryMainPage.class.php';
 
-// Currently loaded via OutputPage::addScript() from MasonryMainPage.class.php
-// due to issues with loading Masonry with ResourceLoader
-/*
-$wgResourceModules['ext.masonrymainpage.deps'] = array(
-	'scripts' => array(
-		'imagesloaded.pkgd.js',
-		'masonry.pkgd.js'
-	),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MasonryMainPage',
-	'position' => 'top',
-);
-*/
+	# Internationalization
+	$wgExtensionMessagesFiles['MasonryMainPageMagic'] = __DIR__ . '/Magic.php';
 
-$wgResourceModules['ext.masonrymainpage.base'] = array(
-	'scripts' => array( 'masonry-common.js' ),
-	'styles' => 'Masonry.css',
-	//'dependencies' => 'ext.masonrymainpage.deps',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MasonryMainPage',
-	'position' => 'top',
-);
+	# The "class" file contains the bulk of a simple parser function extension.
+	$wgAutoloadClasses['MasonryMainPage'] = __DIR__ . '/MasonryMainPage.class.php';
 
-# This specifies the function that will initialize the parser function.
-$wgHooks['ParserFirstCallInit'][] = 'MasonryMainPage::setup';
+	$wgResourceModules['ext.masonrymainpage.base'] = array(
+		'scripts' => array( 'imagesloaded.pkgd.js', 'masonry.pkgd.js', 'masonry-common.js' ),
+		'styles' => 'Masonry.css',
+		'localBasePath' => __DIR__,
+		'remoteExtPath' => 'MasonryMainPage',
+		'position' => 'top',
+	);
+
+	# This specifies the function that will initialize the parser function.
+	$wgHooks['ParserFirstCallInit'][] = 'MasonryMainPage::setup';
+
+}
